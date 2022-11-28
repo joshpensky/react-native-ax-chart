c93f0f3bea35a6ab2c3ebe688d70466ae9c7e921
Josh Pensky <joshuapensky@gmail.com>
2022-11-28 09:37:56 -0500

    chore: initial commit

---
+++ android/src/main/java/com/axchart/AxChartViewManager.kt
@@ -0,0 +1,20 @@
+package com.axchart
+
+import android.graphics.Color
+import android.view.View
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.annotations.ReactProp
+
+class AxChartViewManager : SimpleViewManager<View>() {
+  override fun getName() = "AxChartView"
+
+  override fun createViewInstance(reactContext: ThemedReactContext): View {
+    return View(reactContext)
+  }
+
+  @ReactProp(name = "color")
+  fun setColor(view: View, color: String) {
+    view.setBackgroundColor(Color.parseColor(color))
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  AxChartExample
+//
+
+import Foundation
